4e59ca498bc7c26aef78bbdb035f562f877591d0
Taha Tesser <tessertaha@gmail.com>
2024-09-01 15:54:37 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Taha Tesser on 01.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
