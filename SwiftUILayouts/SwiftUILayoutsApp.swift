f01b464d3fdba0e13e6d0e80a4815781e8142fb4
Taha Tesser <48603081+TahaTesser@users.noreply.github.com>
2025-11-29 11:41:54 +0200

    Initial Commit

---
+++ SwiftUILayouts/SwiftUILayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayoutsApp.swift
+//  SwiftUILayouts
+//
+//  Created by Taha Tesser on 29.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
